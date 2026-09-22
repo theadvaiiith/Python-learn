0a8043724625d116a25f40fd0eaeee8b1d1a8bd1
Advaith Krishna <iamadvaith2004@gmail.com>
2026-09-22 15:40:16 +0530

    new commit of a fucking probelm ie awsome and i did without brushing

---
+++ new_problem_if_even-square,if_odd-keep.py
@@ -0,0 +1,12 @@
+numbers = [3, 8, 12, 5, 17, 20]
+
+updated_list = []
+
+for char in numbers:
+    if char % 2 == 0:
+        char = char ** 2
+        updated_list.append(char)
+    else:
+        char == char
+        updated_list.append(char)
+print(updated_list)
